0d9fde608e01cc892121280181b95754974e91fc
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-26 19:18:55 +0200

    edit step 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
   
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+        console.log("ok ")
     else 
         console.log("Error occurred, server can't start", error);
     }
