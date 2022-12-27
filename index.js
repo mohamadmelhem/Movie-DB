1a6ceef5f7abd3046bcd831acc1fdb4e8c707436
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-27 21:52:05 +0200

    step 3

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ app.get('/' , (req, res )=> {
     res.send('ok');
 });
 
+
+app.get  ('/test' , function(req, res){
+    res.status(200).send({status:200, message:'ok'});
+
+});
+app.get('/time', function(req, res){
+    let time= new Date();
+    res.status(200).send({status:200, message:`${time.getHours()}:${time.getMinutes()}}`})
+})
 app.listen(PORT, () => console.log(`server in now listening on port ${PORT}`))
      
