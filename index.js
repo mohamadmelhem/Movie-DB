d2b3a0c5a53a1d1bd673719f72095f8c6ae2e186
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-28 11:46:15 +0200

    step 4

--- index.js
+++ index.js
@@ -17,5 +17,15 @@ app.get('/time', function(req, res){
     let time= new Date();
     res.status(200).send({status:200, message:`${time.getHours()}:${time.getMinutes()}}`})
 })
+app.get('/hello/:userID', (req, res)=>{
+    res.status(200).send({status:200, message:`Hello ${req.params.userID}`})
+});
+app.get('/search', (req, res)=>{
+    if(req.query.s){
+        res.status(200).send({status:200, message:"ok", data:req.query.s})
+    }else{
+        res.status(500).send({status:500, error:true, message:"you have to provide a search"})
+        }
+});
 app.listen(PORT, () => console.log(`server in now listening on port ${PORT}`))
      
